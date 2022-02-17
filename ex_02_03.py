153fe2bd3a839488911fabdb93d3ceeb90c375bd
Deontey Townsend <65357899+dtown140@users.noreply.github.com>
2022-02-16 22:51:07 -0500

    first commit

--- ex_02_03.py
+++
@@ -1,4 +0,0 @@
-hrs = input('Enter hours: ')
-rate = input('Enter rate: ')
-xp = float(hrs)*float(rate)
-print ("Pay:", xp)
---
+++ ex_03_01.py
@@ -0,0 +1,15 @@
+hrs = input('Enter hours: ')
+rate = input('Enter rate: ')
+fh = float(hrs)
+fr = float(rate)
+#print(fh, fr)
+if fh > 40 :
+    #print('Overtime')
+    reg = fr * fh
+    otp = (fh - 40.0) * (fr * 0.5)
+    #print(reg,otp )
+    xp = reg + otp
+else:
+    #print ('Regular')
+    xp = fh * fr
+print ("Pay:", xp)
